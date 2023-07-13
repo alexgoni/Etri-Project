dd74348c444110f518f80f7df9e6aebcdce2ba1e
yjchoi727 <139309548+yjchoi727@users.noreply.github.com>
2023-07-13 19:42:36 +0900

    Update login.js

--- app/src/pages/api/login.js
+++ app/src/pages/api/login.js
@@ -1,44 +1,39 @@
-import jwt from "jsonwebtoken";
-import connection from "./DB";
+import { sequelize, User } from './DB';
+import jwt from 'jsonwebtoken';
+import performanceNow from 'performance-now';
+
 
 export default function login(req, res) {
-  if (req.method === "POST") {
-    const { email, password } = req.body;
-    const query = `SELECT * FROM users WHERE username = ? AND password = ?`;
-    connection.query(query, [email, password], (err, result) => {
-      if (err) {
-        console.error("Failed to log in:", err);
-        res
-          .status(500)
-          .json({ message: "Failed to log in. Please try again." });
-        return;
-      }
+  if (req.method === 'POST') {
+    const { email, password, building } = req.body;
 
-      if (result.length === 0) {
-        res.status(401).json({ message: "Invalid email or password." });
-      }
+    // 시작 시간 측정
+    const startTime = performanceNow();
 
-      const user = result[0];
+    User.findOne({ where: { username: email, password, building } })
+      .then((user) => {
+        if (!user) {
+          res.status(401).json({ message: 'Invalid email or password.' });
+          return;
+        }
 
-      // Access Token 생성
-      const accessKey = `${process.env.NEXT_PUBLIC_ACCESS_TOKEN_SECRET}`; // Access Token에 사용할 비밀 키
-      const accessToken = jwt.sign({ email: user.username }, accessKey, {
-        expiresIn: "30m",
+        // JWT 생성
+        const secretKey = `${process.env.NEXT_PUBLIC_SECRET_KEY}`;
+        const token = jwt.sign({ email: user.username }, secretKey);
+
+        // login 성공시 쿠키 발행
+        res.setHeader('Set-Cookie', `token=${token}; Path=/; Max-Age=3600`);
+        res.status(200).json({ message: 'Login successful!' });
+
+        // 종료 시간 측정 및 소요 시간 계산
+        const endTime = performanceNow();
+        const elapsedTime = endTime - startTime;
+        console.log(`Database request took ${elapsedTime.toFixed(3)} milliseconds`);
+      })
+      .catch((err) => {
+        console.error('Failed to log in:', err);
+        res.status(500).json({ message: 'Failed to log in. Please try again.' });
       });
-
-      // Refresh Token 생성
-      const refreshKey = `${process.env.NEXT_PUBLIC_REFRESH_TOKEN_SECRET}`; // Refresh Token에 사용할 비밀 키
-      const refreshToken = jwt.sign({ email: user.username }, refreshKey, {
-        expiresIn: "14d",
-      });
-
-      // 로그인 성공 시 토큰 발행
-      res.setHeader("Set-Cookie", [
-        `accessToken=${accessToken}; Path=/; Max-Age=1800`,
-        `refreshToken=${refreshToken}; Path=/; Max-Age=1209600`,
-      ]);
-      res.status(200).json({ message: "Login successful!" });
-    });
   } else {
     res.status(405).end();
   }
