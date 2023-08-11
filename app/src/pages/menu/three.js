d63f8b06ba06ac783ba8f2e9b2e7736cd05783da
yjchoi727 <139309548+yjchoi727@users.noreply.github.com>
2023-08-11 15:45:21 +0900

    Update three.js

--- app/src/pages/menu/three.js
+++ app/src/pages/menu/three.js
@@ -1,131 +1,47 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "@/components/Navbar";
-import { Canvas } from "@react-three/fiber";
-import { Stage, OrbitControls } from "@react-three/drei";
-import Paprika from "@/components/3Dmodel/Paprika.jsx";
-import Paprika2 from "@/components/3Dmodel/Paprika2.jsx";
-import Paprika3 from "@/components/3Dmodel/Paprika3.jsx";
+
+import Level1 from "@/components/3Dmodel/gltf_exports/level1.jsx";
+import Level2 from "@/components/3Dmodel/gltf_exports/level2.jsx";
+import Level4 from "@/components/3Dmodel/gltf_exports/level4.jsx";
+import Level8 from "@/components/3Dmodel/gltf_exports/level8.jsx";
+import Level9 from "@/components/3Dmodel/gltf_exports/level9.jsx";
+import Level11 from "@/components/3Dmodel/gltf_exports/level11.jsx";
+import Level12 from "@/components/3Dmodel/gltf_exports/level12.jsx";
+import Level14 from "@/components/3Dmodel/gltf_exports/level14.jsx";
+import Level16 from "@/components/3Dmodel/gltf_exports/level16.jsx";
+import Level17 from "@/components/3Dmodel/gltf_exports/level17.jsx";
+import Level19 from "@/components/3Dmodel/gltf_exports/level19.jsx";
+import Level20 from "@/components/3Dmodel/gltf_exports/level20.jsx";
+
+const levels = [
+  Level1,
+  Level2,
+  Level4,
+  Level8,
+  Level9,
+  Level11,
+  Level12,
+  Level14,
+  Level16,
+  Level17,
+  Level19,
+  Level20,
+];
 
 function Three() {
-  const numPaprikas = 3; // 파프리카의 개수
-  const [currentPaprikaIndex, setCurrentPaprikaIndex] = useState(0);
-
-  // 슬라이드 바를 조작할 때 호출되는 함수
-  const handleSliderChange = (event) => {
-    const newIndex = parseInt(event.target.value, 10);
-    setCurrentPaprikaIndex(newIndex);
-  };
-
-  // Array of different paprika models
-  const paprikaModels = [Paprika, Paprika2, Paprika3];
-
-  // Get the currently selected model
-  const CurrentPaprikaModel = paprikaModels[currentPaprikaIndex];
-
   return (
     <>
       <Navbar />
       <div className="h-80">
-        <Canvas>
-          <Stage environment="city" intensity={0.6}>
-            {/* Render the current paprika model based on the currentPaprikaIndex */}
-            <group>
-              <CurrentPaprikaModel />
-            </group>
-          </Stage>
-          <OrbitControls enableZoom={false} />
-        </Canvas>
+        {levels.map((Level, index) => (
+          <div key={index} className="h-full">
+            <Level />
+          </div>
+        ))}
       </div>
-      {/* 슬라이드 바를 사용하여 파프리카 모델 변경 */}
-      <input
-        className="mx-auto block"
-        type="range"
-        min={0}
-        max={numPaprikas - 1}
-        value={currentPaprikaIndex}
-        onChange={handleSliderChange}
-      />
     </>
   );
 }
 
 export default Three;
-
-// 시간순
-// import React, { useState, useEffect } from "react";
-// import { useTransition, animated } from "react-spring";
-// import Navbar from "@/components/Navbar";
-// import { Canvas } from "@react-three/fiber";
-// import { Stage, OrbitControls } from "@react-three/drei";
-// import Paprika from "@/components/3Dmodel/Paprika.jsx";
-// import Greenhouse from "@/components/3Dmodel/Greenhouse.jsx";
-
-// function Three() {
-//   const models = [
-//     "paprika",
-//     "greenhouse",
-//     "paprika",
-//     "paprika",
-//     "greenhouse" /*...and so on...*/,
-//   ];
-//   const [currentModelIndex, setCurrentModelIndex] = useState(0);
-
-//   // 모델 변경하는 함수
-//   const changeModel = () => {
-//     setCurrentModelIndex((prevIndex) => (prevIndex + 1) % models.length);
-//   };
-
-//   // 애니메이션 설정
-//   const transitions = useTransition(models[currentModelIndex], {
-//     from: { opacity: 0 },
-//     enter: { opacity: 1 },
-//     leave: { opacity: 0 },
-//     config: { duration: 1000 }, // 애니메이션 지속 시간 (밀리초)
-//   });
-
-//   // 시간 간격으로 모델 변경
-//   useEffect(() => {
-//     const timeoutId = setTimeout(changeModel, 3000);
-
-//     return () => clearTimeout(timeoutId);
-//   }, [currentModelIndex]);
-
-//   return (
-//     <>
-//       <Navbar />
-//       <div className="h-80">
-//         {/* Render the Canvas component */}
-//         <Canvas>
-//           <Stage environment="city" intensity={0.6}>
-//             {/* Use the ternary operator to conditionally render models */}
-//             {models.map((model, index) => (
-//               <React.Fragment key={index}>
-//                 {model === "paprika" && (
-//                   <Paprika visible={currentModelIndex === index} />
-//                 )}
-//                 {model === "greenhouse" && (
-//                   <Greenhouse visible={currentModelIndex === index} />
-//                 )}
-//               </React.Fragment>
-//             ))}
-//           </Stage>
-//           <OrbitControls enableZoom={false} />
-//         </Canvas>
-//       </div>
-//       {/* Render the transitions outside the Canvas */}
-//       {transitions((styles, item) =>
-//         item === "paprika" ? (
-//           <animated.div style={styles}>
-//             <Paprika />
-//           </animated.div>
-//         ) : (
-//           <animated.div style={styles}>
-//             <Greenhouse />
-//           </animated.div>
-//         )
-//       )}
-//     </>
-//   );
-// }
-
-// export default Three;
